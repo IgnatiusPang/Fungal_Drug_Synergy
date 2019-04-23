1f611fa68d9871e6690dcdf550549f11e8932d8b
IgnatiusPang <i.pang@unsw.edu.au>
2019-04-23 18:26:11 +1000

    Compare enriched GO terms between species

--- compare_cneo_scer/Source/Common/global_parameters.R
+++ compare_cneo_scer/Source/Common/global_parameters.R
@@ -1,14 +1,26 @@
 
-
+base_dir    <- "/home/ignatius/PostDoc/2019/Fungal_Drug_Synergy"
 
 ## List of directories
-base_dir    <- "/home/ignatius/PostDoc/2019/Fungal_Drug_Synergy/compare_cneo_scer"
-data_dir    <- file.path ( base_dir, "Data" ) 
-source_dir  <- file.path( base_dir, "Source")
-common_dir  <- file.path( source_dir, "Common")  # Commonly used codes (e.g. parameters file)
-results_dir <- file.path( base_dir, "Results")
+compare_base_dir    <- file.path(base_dir, "compare_cneo_scer" )
+compare_data_dir    <- file.path ( compare_base_dir, "Data" ) 
+compare_source_dir  <- file.path( compare_base_dir, "Source")
+compare_common_dir  <- file.path( compare_source_dir, "Common")  # Commonly used codes (e.g. parameters file)
+compare_results_dir <- file.path( compare_base_dir, "Results")
 
+# C. neoformans directories
+cneo_base_dir    <- file.path( base_dir, "cneo_synergy" )
+cneo_data_dir    <- file.path( cneo_base_dir, "Data" ) 
+cneo_source_dir  <- file.path( cneo_base_dir, "Source")
+cneo_common_dir  <- file.path( cneo_source_dir, "Common")  # Commonly used codes (e.g. parameters file)
+cneo_results_dir <- file.path( cneo_base_dir, "Results")
 
+# S. cerevisiae directories
+scer_base_dir    <- file.path( base_dir, "scer_synergy" )
+scer_data_dir    <- file.path( scer_base_dir, "Data" ) 
+scer_source_dir  <- file.path( scer_base_dir, "Source")
+scer_common_dir  <- file.path( scer_source_dir, "Common")  # Commonly used codes (e.g. parameters file)
+scer_results_dir <- file.path( scer_base_dir, "Results")
 
 
 
